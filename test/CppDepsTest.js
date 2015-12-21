4b52d462309eee84e1b2c6764abf9eb75d5c9c0d
souzabrizolara <souzabrizolara@gmail.com>
2015-12-21 19:06:24 -0300

    Tests for dependencies parser

---
+++ test/CppDepsTest.js
@@ -0,0 +1,59 @@
+var should = require('should')
+var path = require('path')
+
+CppDeps = require('../lib/CppDeps.js')
+
+PKG_REL_PATH = './test/installed_pkg';
+PKG_ABSPATH  = path.resolve(PKG_REL_PATH),
+SUBPKGS_PATH = path.join(PKG_REL_PATH, 'node_modules')
+
+SUBPKGS = {
+    'subpkg1' : ['incdir', 'libdir', 'srcdir'],
+    'other'   : ['include', null, 'src']
+}
+
+PKG_DEPS = {}
+
+for(subp in SUBPKGS){
+    pkgDirs = {};
+
+    pkgPath = path.join(SUBPKGS_PATH, subp);
+    dirs = ['include', 'lib', 'src']
+
+    for(i=0; i < dirs.length; ++i){
+        if(SUBPKGS[subp][i]){
+            pkgDirs[dirs[i]] = SUBPKGS[subp][i]; //path.join(pkgPath, SUBPKGS[subp][i])
+        }
+    }
+
+    PKG_DEPS[subp] = pkgDirs;
+}
+
+describe("CppDeps", function(){
+    cppdeps = CppDeps();
+
+    describe("#findDependenciesPaths", function(){
+        context("given some installed package", function(){
+            installedPackage = './test/installed_pkg'
+
+            it("it should travel the dependencies of that package and return a list of their cpp dirs"
+            , function(done){
+                cppdeps.findDependenciesPaths(installedPackage, function(err, depsPaths){
+                    console.log("depsPaths:", depsPaths)
+                    depsPaths.should.be.an.Object;
+                    depsPaths.should.not.be.an.Array;
+
+                    for(pkgDep in PKG_DEPS){
+                        expected = PKG_DEPS[pkgDep]
+                        dPath = depsPaths[pkgDep]
+                        console.log("\"", dPath, "\" == ", PKG_DEPS[pkgDep] )
+                        dPath.should.have.properties(expected);
+                    }
+
+
+                    done();
+                });
+            });
+        });
+    });
+});
